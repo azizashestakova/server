4ea0fdccaa4af98755ac1b90a64f7654281ee677
Aziza <aziza.shestakova@nutnet.ru>
2020-09-26 14:27:04 +0400

    feat: get/post запросы

--- routes/tasks.js
+++ routes/tasks.js
@@ -1,24 +1,31 @@
-// var express = require('express');
-// var router = express.Router();
-
-// router.get('/', function(req, res, next) {
-//   res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
-//   res.json(req);
-// });
-
-// module.exports = router;
-
-
 var express = require('express');
 var router = express.Router();
+const tasks = [
+  {
+    title: 'title1',
+    id: 'id1',
+    completed: true
+  },
+  {
+    title: 'title2',
+    id: 'id2',
+    completed: false
+  }
+]
+
+// router.post('/', function(req, res, next) {
+//     res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+//     console.log(2, req);
+//     res.send('POST request to the homepage');
+// });
 
 router.post('/', function(req, res, next) {
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
-    console.log(1, req.body);
-    res.send('POST request to the homepage');
-//   console.log(req.body);
-//   console.log(req.body.task.id);
-  // res.send('respond with a resource');
+  console.log(1, req.body);
+});
+
+router.get('/', function(req, res, next) {
+    // console.log(1, req.body);
+    res.json(tasks);
 });
 
 module.exports = router;
