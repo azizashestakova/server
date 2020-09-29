03496c2e066c419dba09ee12791e56baf1298527
Aziza <ashestakova@htc-cs.ru>
2020-09-29 16:47:19 +0400

    fix: сохранять созданные задачи

--- routes/tasks.js
+++ routes/tasks.js
@@ -1,30 +1,12 @@
 var express = require('express');
 var router = express.Router();
-const tasks = [
-  {
-    title: 'title1',
-    id: 'id1',
-    completed: true
-  },
-  {
-    title: 'title2',
-    id: 'id2',
-    completed: false
-  }
-]
-
-// router.post('/', function(req, res, next) {
-//     res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
-//     console.log(2, req);
-//     res.send('POST request to the homepage');
-// });
+let tasks = [];
 
 router.post('/', function(req, res, next) {
-  console.log(1, req.body);
+  tasks = req.body;
 });
 
 router.get('/', function(req, res, next) {
-    // console.log(1, req.body);
     res.json(tasks);
 });
 
