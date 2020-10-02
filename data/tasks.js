7c2048a0fcdc26cc0b0169e1c7a86f132de48219
Aziza <ashestakova@htc-cs.ru>
2020-10-02 11:35:43 +0400

    fix: вынесла функцию создания json файла отдельно

--- data/tasks.js
+++ data/tasks.js
@@ -1,4 +1,5 @@
-var fs = require('fs');
+const fs = require('fs')
+const path = './data/tasks.json'
 
 module.exports.saveTasks = function(tasks, callback) {
     fs.writeFile('./data/tasks.json', JSON.stringify(tasks, null, '\t'), callback);
@@ -6,4 +7,18 @@ module.exports.saveTasks = function(tasks, callback) {
 
 module.exports.getTasks = function(callback) {
     fs.readFile('./data/tasks.json', 'utf8', callback);
+}
+
+module.exports.isExist = function() {
+    fs.access(path, fs.F_OK, (err) => { // проверяем существует ли путь до файла
+        if (err) { // если нет, то нужно создать
+            fs.open(path, 'w', (err) => { // создаем пустой файл
+                if(err) throw err;
+            });
+            
+            fs.appendFile(path, '[]', function (err) { // добавляем в файл пустой массив
+                if (err) throw err;
+            });
+        }
+    })
 }
--- routes/tasks.js
+++ routes/tasks.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const tasks = require('../data/tasks.js');
-const fs = require('fs')
-const path = './data/tasks.json'
+
+tasks.isExist(); // проверяем существует ли файл tasks.json
 
 router.post('/', function(req, res, next) {
   tasks.saveTasks(req.body, function(err) { // перезаписываем массив с задачами
@@ -14,23 +14,12 @@ router.post('/', function(req, res, next) {
 });
 
 router.get('/', function(req, res, next) {
-  fs.access(path, fs.F_OK, (err) => { // проверяем существует ли путь до файла
-    if (err) { // если нет, то нужно создать
-      fs.open(path, 'w', (err) => { // создаем пустой файл
-        if(err) throw err;
-      });
-      
-      fs.appendFile(path, '[]', function (err) { // добавляем в файл пустой массив
-        if (err) throw err;
-      });
-    }
-    tasks.getTasks(function(error, data) { // получаем задачи
-      if (error) {
-        throw error
-      };
-      res.send(data);
-    });
-  })
+  tasks.getTasks(function(error, data) { // получаем задачи
+    if (error) {
+      throw error
+    };
+    res.send(data);
+  });
 });
 
 
